dbc5a4a5caaeb26d2b282974461aacf7d18b4f23
Varujan Ikilikyan <varujanna@gmail.com>
2026-05-30 07:08:50 +0400

    traying sequalize

--- clients/db.mysql.js
+++ clients/db.mysql.js
@@ -8,18 +8,3 @@ const {
     MY_SQL_DATABASE,
 } = process.env;
 
-const connection = mysql.createConnection({
-    host: MY_SQL_HOST,
-    port: MY_SQL_PORT,
-    user: MY_SQL_USER,
-    password: MY_SQL_PASSWORD,
-    database: MY_SQL_DATABASE,
-});
-
-connection.connect(function (err, db) {
-    if (!err) {
-        console.log('DB connection succeeded.');
-    } else console.log(err, db)
-});
-
-export default connection.promise();
